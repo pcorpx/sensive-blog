30c1f644411f00491d825a67f703455132560e4a
pcorpx <pcorpx@gmail.com>
2021-12-20 11:16:11 +0500

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,9 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all().
+            annotate(posts_count=Count('posts'))
+        ],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -37,6 +39,12 @@ def serialize_tag(tag):
     return {
         'title': tag.title,
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
+    }
+
+def serialize_tag_optimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts_count,
     }
 
 
@@ -53,7 +61,9 @@ def index(request):
     )
     most_fresh_posts = fresh_posts[:5]
 
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = (Tag.objects.popular()[:5].
+        annotate(posts_count=Count('posts'))
+    )
 
     context = {
         'most_popular_posts': [
@@ -62,7 +72,7 @@ def index(request):
         'page_posts': [
             serialize_post_optimized(post) for post in most_fresh_posts
         ],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
 
